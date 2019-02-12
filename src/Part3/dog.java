d19eb86407a0ad52ead231876a4e5f08a4e46f1c
Ilya <Ilya.Shpektorov@aplana.com>
2019-02-12 20:05:26 +0300

    тренировка ООП

---
+++ src/Part3/dog.java
@@ -0,0 +1,17 @@
+package Part3;
+
+public class dog {
+ String name;
+ 
+ public  void bark() {
+	System.out.println("Я " + name);
+	System.out.println("brr");
+}
+ public  void eat() {
+		
+}
+ public  void chaseCat() {
+		
+}
+ 
+}
